60cd5c45dc8ff3793444bcaa40be0adc41212a9a
Rana Dedar <rajpotdedar@gmail.com>
2026-03-15 19:58:13 +0500

    merged with feature/statistics

--- student_manager.py
+++ student_manager.py
@@ -1,11 +1,7 @@
 from student import Student
-from file_handler import FileHandler
 class Student_manager:
     def __init__ (self):
-        self.handler = FileHandler()
-        self.students = self.handler.load_data()
-    def _autosave(self):
-        self.handler.save_data(self.students)
+        self.students=[]
     def add_student(self,student_id,name,age,email):
         if any(s.student_id==student_id  for s in self.students):
             print("the student already exist with",student_id)
@@ -13,7 +9,6 @@ class Student_manager:
         new_student=Student(student_id,name,age,email,grades=None)
         self.students.append(new_student)
         print(f"The student named :: {name} has been added")
-        self._autosave()
     def search_student(self,querry):
         querry=str(querry).lower()
         result=[]
@@ -32,7 +27,6 @@ class Student_manager:
             if s.student_id ==student_id:
                 self.students.remove(s)
                 print(f"student with {student_id} is removed")
-                self._autosave()
                 return True
             print(f"no student with {student_id} found")
             return False
@@ -52,17 +46,41 @@ class Student_manager:
             else:
                 print(f"the attribute doesn't exist for the student {student_id}")
         print(f"the Student {student_id} has been updated successfully")
-        self._autosave()
         return True
     def list_all_students(self):
-        for student in self.students:
-            student.display_info()
+        if not self.students:
+            print("there are no student")
+            return
+        for s in self.students:
+            s.display_info()
+    def get_stats(self):
+        if not self.students:
+            print("there are no students yet")
+            return False
+        avg=[s.get_avg() for s in self.students]
+        stats={
+            "total_students" : len(self.students),
+            "Class_Average" :sum(avg)/len(avg),
+            "Highest_Score" :max(avg),
+            "Lowest_Score" :min(avg),
+            "pass_rate" :(len([a for a in avg if a>=50])/len(avg))*100
+        }
+        for key,value in stats.items():
+            unit="%" if "Average" in key or "rate" in key else ""
+            print(f"{key} : {value:2f}{unit}")
+        return stats
+
     def get_top_students(self,TS):
         if not self.students:
             print("There are no students yet")
             return []
         sorted_students=sorted(self.students,key=lambda s: s.get_avg(),reverse=True)
-                
+        top_s=sorted_students[:TS]
+        print(f"Top {len(top_s)} Students")
+        for s in top_s:
+            print(f"{s.name} :: {s.get_avg():.2f}")
+        return top_s   
+        
             
         
 
